d048f434380b3e1a9db707ec9757d91b71931ce9
Luis <114741601+luisdeharoparames@users.noreply.github.com>
2022-12-04 23:47:45 +0100

    Add files via upload

---
+++ PracticaJavaScript.js
@@ -0,0 +1,85 @@
+/*
+
+Accessing a variable (1)
+
+
+
+En este sencillo ejercicio declaramos una variable llamada num y le asignamos un valor de 5.
+
+Luego tratamos de registrar el valor de la variable usando el console.log() método.
+
+Pero, el console.log() método contiene un pequeño error.
+
+Si intenta ejecutar el código, verá un mensaje de error.
+
+Corrija el error.
+
+*/
+
+// const num = 5;
+
+// console.log(num);
+
+/*
+
+Conectar cadenas Javascript (1)
+
+Una vez que hayamos aprendido a crear cadenas JavaScript, ahora conectaremos 2 cadenas.
+
+En el siguiente código, usamos el operador Adición (+) para concatenar textOne y textTwo.
+
+La console.log() instrucción registra la cadena resultante.
+
+Actualmente, el resultado sería HelloWorld.
+
+Cambie el código a continuación para que el valor de combined sea Hello World
+
+*/
+
+
+
+// const textOne = 'Hello';
+
+// const textTwo = 'World';
+
+// const combined = textOne + " " +  textTwo;
+
+// console.log(combined);
+
+
+/*
+
+Operadores aritméticos - Exponencial
+
+En el siguiente código, calculamos numOne a la potencia de numTwo con el operador exponencial **.
+
+Luego, la console.log() declaración verifica si el resultado de este cálculo es igual a 8.
+
+Cambie el código para que la console.log() instrucción se registre true.
+
+*/
+
+
+
+// const numOne = 2;
+
+// const numTwo = 3;
+
+// const res = numOne ** numTwo;
+
+// console.log(res == 8);
+const MONTHS = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
+
--- main.js
+++ main.js
@@ -1,48 +1,69 @@
+const MONTHS = {
+  enero: 0,
+  febrero: 1,
+  marzo: 2,
+  abril: 3,
+  mayo: 4,
+  junio: 5,
+  julio: 6,
+  agosto: 7,
+  septiembre: 8,
+  octubre: 9,
+  noviembre: 10,
+  diciembre: 11,
+};
+
+const locale = "es";
+
 (() => {
   const form = document.getElementById("myForm");
-
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
-    const { from } = Object.fromEntries(formData);
-    const { to } = Object.fromEntries(formData);
-    console.log(from);
-    console.log(to);
-    createTables(from,to);
-    //let td = [];
+    // console.log(formData);
+    const { month } = Object.fromEntries(formData);
+    const { isExists, month: monthName } = monthExists(month);
+    if (isExists) {
+      createTable(monthName, 2022);
+    }else {
+      const div = document.getElementById("renderTable");
+      div.innerText = "El dato introducido no es un mes"
+    }
   });
-  const createTables = (from,to) => {
-    const div = document.getElementById("renderTable");
-    div.classList.add("divCSS");
-    for (let numeroFrom = from; numeroFrom <= to; numeroFrom++) {
-      div.append(createTable(numeroFrom));
-    }
 
-  }
-
-  const createTable = (numero) => {
-    const table = document.createElement("table");
-    table.classList.add("tableCSS");
-
-    for (let i = 0; i < 10; i++) {
-      table.appendChild(createRom(numero,i));
-    }
-    return table;
+  const monthExists = (month) => {
+    month = MONTHS[month.toLowerCase()];
+    return {
+      isExists: month !== undefined && month >= 0 && month <= 11,
+      month,
+    };
   };
-  //Crear Celdas
-  const createCell = (innerText) => {
-    const td = document.createElement("td");
-    td.classList.add("tdCSS");
-    td.innerText = innerText;
-    return td;
+
+  const createTable = (month, year) => {
+    const div = document.getElementById("renderTable");ss
+    div.innerText = "";
+    // const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+    const container = document.createElement("div");
+    container.classList.add("boxCalendar");
+    const intl = new Intl.DateTimeFormat(locale, { month: "long" });
+    const monthName = intl.format(new Date(year, month))
+    const title = document.createElement("h1")
+    title.innerText = `${monthName} ${year}`
+    div.append(title)
+    getDaysWeek(locale, container);
+    div.append(container);
   };
-  //Crear tr
-  const createRom = (numero,i) => {
-    const tr = document.createElement("tr");
-    [numero, " X ", i, " = ", numero * i].forEach((element) => {
-      tr.appendChild(createCell(element));
+
+
+
+  const getDaysWeek = (locale, container) => {
+    const intl = new Intl.DateTimeFormat(locale, { weekday: "long" });
+    [...Array(7).keys()].map((day) => {
+      const dayBox = document.createElement("div");
+      dayBox.classList.add("day-name");
+      dayBox.innerText = intl.format(new Date(1973, 0, day + 1));
+      container.append(dayBox);
     });
-    return tr;
   };
 })();
